ef16a947f4a4e15c1c69587713741029ef4999e0
boaltamirano <brayanaltamiranoperez@gmail.com>
2023-09-13 13:27:59 -0500

    feat: movements services

---
+++ src/main/java/com/omar/omar/service/MovesService.java
@@ -0,0 +1,43 @@
+package com.omar.omar.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.omar.omar.model.Moves;
+import com.omar.omar.repository.MoveRepository;
+
+@Service
+public class MovesService {
+    
+    @Autowired
+    private MoveRepository movementRepository;
+
+    public Moves createMoves(Moves movement) {
+
+        return movementRepository.save(movement);
+    }
+
+
+    public List<Moves> getAllMovements() {
+        return (List<Moves>) movementRepository.findAll();
+    }
+
+
+    public Moves getMovementById(Long movementId) {
+        Moves movement = movementRepository.getReferenceById(movementId);
+        if (movement != null) {
+            return movement;
+        }
+        return null;
+    }
+
+
+    @Transactional
+    public void deleteMoves(Long movementId) {
+        movementRepository.deleteById(movementId);
+    }
+
+}
